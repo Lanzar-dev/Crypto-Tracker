54e60ba7713d59046030e1938f2be1da2727ad45
System Administrator <admin@MngEnvMCAP472507.onmicrosoft.com>
2025-03-17 15:04:39 +0000

    Updated update-image.sh

--- scripts/update-image.sh
+++ scripts/update-image.sh
@@ -11,6 +11,8 @@ NEW_IMAGE_TAG="$CONTAINER_REGISTRY/$IMAGE_REPOSITORY:$TAG"
 
 echo "Updating Kubernetes manifests with new image tag: $NEW_IMAGE_TAG"
 
+git checkout master
+
 # Update image in Kubernetes manifests
 sed -i "s|image: cryptotrackeracr.azurecr.io/cryptotracker:.*|image: $NEW_IMAGE_TAG|g" $K8S_MANIFEST_DIR/*.yaml
 
@@ -19,6 +21,6 @@ echo "Committing and pushing changes to Git..."
 # Commit and push changes
 git add .
 git commit -m "Update image tag to $NEW_IMAGE_TAG"
-git push
+git push origin master
 
 echo "Changes pushed successfully! ArgoCD will now deploy the update."
